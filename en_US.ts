4f667c44074b7c641677d556f3de7ad9b46aefb2
Purple-CSGO <438518244@qq.com>
2020-03-18 23:40:22 +0800

    add multilanguage support

---
+++ en_US.ts
@@ -0,0 +1,152 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="26"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="56"/>
+        <source>生成</source>
+        <translation type="unfinished">Generate</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="74"/>
+        <source>3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="95"/>
+        <source>2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="116"/>
+        <source>-1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="137"/>
+        <source>1920</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="158"/>
+        <source>1080</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="182"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="204"/>
+        <source>size</source>
+        <translation type="unfinished">size: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="226"/>
+        <source>thickness</source>
+        <translation type="unfinished">thickness: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="248"/>
+        <source>gap</source>
+        <translation type="unfinished">gap: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="276"/>
+        <source>outline</source>
+        <translation type="unfinished">outline: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="298"/>
+        <source>width</source>
+        <translation type="unfinished">width: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="320"/>
+        <source>height</source>
+        <translation type="unfinished">height: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="21"/>
+        <source>高: </source>
+        <translation type="unfinished">Height: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="22"/>
+        <source>宽: </source>
+        <translation type="unfinished">Width: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="23"/>
+        <source>准星长度: </source>
+        <translation type="unfinished">size: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="24"/>
+        <source>准星间隙: </source>
+        <translation type="unfinished">Gap: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="25"/>
+        <source>准星厚度: </source>
+        <translation type="unfinished">Thickness: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="26"/>
+        <source>准星外轮廓: </source>
+        <translation type="unfinished">Outline: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="31"/>
+        <source>绿色</source>
+        <translation type="unfinished">Green</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="31"/>
+        <source>红色</source>
+        <translation type="unfinished">Red</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="31"/>
+        <source>蓝色</source>
+        <translation type="unfinished">Blue</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="31"/>
+        <source>白色</source>
+        <translation type="unfinished">White</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="31"/>
+        <source>青色</source>
+        <translation type="unfinished">Cyan</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="118"/>
+        <source>请选择保存的位置</source>
+        <translation type="unfinished">Please choose save Path</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="118"/>
+        <source>图片 (*.png)</source>
+        <translation type="unfinished">Image (*.png)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="124"/>
+        <source>提示</source>
+        <translation type="unfinished">Info</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="124"/>
+        <source>保存成功</source>
+        <translation type="unfinished">Save succeeded</translation>
+    </message>
+</context>
+</TS>
---
+++ zh_CN.ts
@@ -0,0 +1,152 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="26"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="56"/>
+        <source>生成</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="74"/>
+        <source>3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="95"/>
+        <source>2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="116"/>
+        <source>-1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="137"/>
+        <source>1920</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="158"/>
+        <source>1080</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="182"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="204"/>
+        <source>size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="226"/>
+        <source>thickness</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="248"/>
+        <source>gap</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="276"/>
+        <source>outline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="298"/>
+        <source>width</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="320"/>
+        <source>height</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="21"/>
+        <source>高: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="22"/>
+        <source>宽: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="23"/>
+        <source>准星长度: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="24"/>
+        <source>准星间隙: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="25"/>
+        <source>准星厚度: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="26"/>
+        <source>准星外轮廓: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="31"/>
+        <source>绿色</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="31"/>
+        <source>红色</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="31"/>
+        <source>蓝色</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="31"/>
+        <source>白色</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="31"/>
+        <source>青色</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="118"/>
+        <source>请选择保存的位置</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="118"/>
+        <source>图片 (*.png)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="124"/>
+        <source>提示</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="124"/>
+        <source>保存成功</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
